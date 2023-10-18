3adc1d14bb7a8cf18f355e3083032bf87d841d4d
Danil Shovgun <shovgun.dan@gmail.com>
2023-10-18 20:04:23 +0500

    Modified main.py

--- main.py
+++ main.py
@@ -1,57 +1,62 @@
 import asyncio
 import aiohttp
-import psycopg2
-from psycopg2 import sql
+import asyncpg
 
 
-connection = psycopg2.connect(database='starwars', user='user', password='password')
-cursor = connection.cursor()
+async def create_table():
+    conn = await asyncpg.connect(database='starwars', user='user', password='password')
+    await conn.execute("""
+    CREATE TABLE IF NOT EXISTS characters (
+        id integer PRIMARY KEY,
+        name text,
+        birth_year text,
+        eye_color text,
+        gender text,
+        hair_color text,
+        height text,
+        homeworld text,
+        mass text,
+        skin_color text,
+        films text,
+        species text,
+        starships text,
+        vehicles text
+    )
+    """)
+    await conn.close()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS characters (
-    id integer PRIMARY KEY,
-    name text,
-    birth_year text,
-    eye_color text,
-    gender text,
-    hair_color text,
-    height text,
-    homeworld text,
-    mass text,
-    skin_color text,
-    films text,
-    species text,
-    starships text,
-    vehicles text
-)
-""")
 
 async def fetch(session, url):
     async with session.get(url) as response:
         return await response.json()
 
+
 async def extract_data(session, id):
     character_data = await fetch(session, f'https://swapi.dev/api/people/{id}/')
-    character_data['films'] = ', '.join([fetch(session, film_url)['title'] for film_url in character_data['films']])
-    character_data['species'] = ', '.join([fetch(session, species_url)['name'] for species_url in character_data['species']])
-    character_data['starships'] = ', '.join([fetch(session, starship_url)['name'] for starship_url in character_data['starships']])
-    character_data['vehicles'] = ', '.join([fetch(session, vehicle_url)['name'] for vehicle_url in character_data['vehicles']])
+    character_data['films'] = ', '.join([await (await fetch(session, film_url))['title'] for film_url in character_data['films']])
+    character_data['species'] = ', '.join([await (await fetch(session, species_url))['name'] for species_url in character_data['species']])
+    character_data['starships'] = ', '.join([await (await fetch(session, starship_url))['name'] for starship_url in character_data['starships']])
+    character_data['vehicles'] = ', '.join([await (await fetch(session, vehicle_url))['name'] for vehicle_url in character_data['vehicles']])
     return character_data
 
+
 async def load_data(session, id):
+    conn = await asyncpg.connect(database='starwars', user='user', password='password')
     character_data = await extract_data(session, id)
     fields = ('id', 'name', 'birth_year', 'eye_color', 'gender', 'hair_color', 'height', 'homeworld', 'mass', 'skin_color', 'films', 'species', 'starships', 'vehicles')
-    query = sql.SQL("""
+    await conn.execute("""
         INSERT INTO characters ({})
         VALUES ({})
         ON CONFLICT (id) DO NOTHING
-    """).format(
-        sql.SQL(',').join(map(sql.Identifier, fields)),
-        sql.SQL(',').join(sql.Placeholder() for _ in fields)
-    )
-    cursor.execute(query, {field: character_data[field] for field in fields})
+    """.format(
+        ','.join(fields),
+        ','.join(f'${i+1}' for i in range(len(fields)))
+    ), [character_data[field] for field in fields])
+    await conn.close()
+
 
 async def main():
+    await create_table()
     async with aiohttp.ClientSession() as session:
         for character_id in range(1, 100):
             try:
@@ -59,8 +64,6 @@ async def main():
             except Exception as e:
                 print(f'Ошибка при обработке ID {character_id}:', e)
 
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
-
-connection.commit()
-connection.close()
